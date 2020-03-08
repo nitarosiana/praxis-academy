83990b6df0008275b6b34ba7ed409e3cf004e3ee
nitarosiana <55678977+nitarosiana@users.noreply.github.com>
2020-03-09 06:17:32 +0700

    Update Tugas2.py

--- Novice/01-03/Kasus/01-03-02/Tugas2.py
+++ Novice/01-03/Kasus/01-03-02/Tugas2.py
@@ -1,1 +1,13 @@
-import math
+import datetime
+import time
+
+waktu = time.time()
+sekarang = datetime.today()
+tanggal = sekarang.date()
+
+print ('Hari = ',tanggal.day)
+print ('Bulan = ',tanggal.month)
+print ('Tahun = ',tanggal.year)
+print ('jam = ',waktu.hour)
+print ('Menit = ',waktu.minute)
+print ('Detik = ',waktu.second)
